1acb7d26ca457a2a9f04d5a50335c29afd0d991c
enaikele omoh kelvin <enaikeleomoh@gmail.com>
2025-03-01 19:35:45 +0100

    updated

--- src/constants/images.ts
+++ src/constants/images.ts
@@ -1,12 +1,12 @@
 export const images = {
     hero: "https://images.unsplash.com/photo-1625246333195-78d9c38ad449?q=80&w=1000&auto=format&fit=crop",
     features: {
-        monitoring: "https://images.unsplash.com/photo-1593113630400-ea4288922497?q=80&w=2070&auto=format&fit=crop",
+        monitoring: "https://extension.umn.edu/sites/extension.umn.edu/files/styles/caption_large/public/portable-capacitance-probe.jpg?itok=2WDFaaXc",
         ai: "https://images.unsplash.com/photo-1581091226825-a6a2a5aee158?q=80&w=2070&auto=format&fit=crop",
         market: "https://images.unsplash.com/photo-1619566636858-adf3ef46400b?q=80&w=2070&auto=format&fit=crop",
-        mobile: "https://images.unsplash.com/photo-1530541930197-ff16ac917b0e?q=80&w=2070&auto=format&fit=crop",
-        community: "https://images.unsplash.com/photo-1569880153113-76e33fc52d5f?q=80&w=2070&auto=format&fit=crop",
-        analytics: "https://images.unsplash.com/photo-1590682680695-43b964a3ae17?q=80&w=2070&auto=format&fit=crop"
+        mobile: "https://londoncg.com/hs-fs/hubfs/Imagenes%20MS%20(blog)/Agricultura%20de%20precision/considera-las-metricas-clave.png?width=900&name=considera-las-metricas-clave.png",
+        community: "https://thisoldcity.com/wp-content/uploads/2022/01/images2247-61eff5f4d31cb-768x493.jpg",
+        analytics: "https://onlinedegrees.sandiego.edu/wp-content/uploads/2022/11/what-is-data-analytics-mads.jpg"
     },
     testimonials: {
         farmer1: "https://images.unsplash.com/photo-1507914372368-b2b085b925a1?q=80&w=2070&auto=format&fit=crop",
